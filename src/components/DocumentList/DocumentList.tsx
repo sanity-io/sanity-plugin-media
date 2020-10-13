9adbd48b4cde3efea8000596bc9f77955f9c95cb
Robin Pyon <robin.pyon@gmail.com>
2020-10-13 12:25:13 +0100

    Force styles on sanity preview components

--- src/components/DocumentList/DocumentList.tsx
+++ src/components/DocumentList/DocumentList.tsx
@@ -2,10 +2,23 @@ import {IntentLink} from 'part:@sanity/base/router'
 import Preview from 'part:@sanity/base/preview'
 import schema from 'part:@sanity/base/schema'
 import React from 'react'
+import styled from 'styled-components'
 
 import Box from '../../styled/Box'
 
 type Props = {documents: any}
+
+// Brute force styles on all of Sanity's preview components.
+
+// TODO: Consider using a custom preview component that is able to resolve custom titles
+// (and potentially subtitles) defined at the document schema level. Or anything to ensure
+// that future upstream changes to Sanity's preview components don't break anything here.
+const Container = styled(Box)`
+  * {
+    color: ${props => props.theme.colors.lighterGray};
+    font-size: ${props => props.theme.fontSizes?.[1]};
+  }
+`
 
 const DocumentList = (props: Props) => {
   const {documents} = props
@@ -22,11 +35,11 @@ const DocumentList = (props: Props) => {
         }
 
         return (
-          <Box key={doc._id}>
+          <Container key={doc._id} mb={1}>
             <IntentLink intent="edit" params={{id: doc._id}} key={doc._id}>
-              <Preview value={doc} type={schemaType} />
+              <Preview layout="default" value={doc} type={schemaType} />
             </IntentLink>
-          </Box>
+          </Container>
         )
       })}
     </Box>
