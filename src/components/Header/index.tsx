6f39d284a1fea47ff82818353a1e97d9404e077e
Robin Pyon <robin.pyon@gmail.com>
2021-01-16 23:53:56 +0000

    fix: add text-overflow: ellipsis to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import {Box, Button, Flex, Inline, Text} from '@sanity/ui'
 import React, {FC} from 'react'
 
 import useTypedSelector from '../../hooks/useTypedSelector'
+import TextEllipsis from '../TextEllipsis'
 
 type Props = {
   onClose?: () => void
@@ -19,34 +20,43 @@ const Header: FC<Props> = (props: Props) => {
     <Box paddingY={2}>
       <Flex align="center" justify="space-between">
         {/* Label */}
-        <Box marginX={3}>
-          <Inline space={2}>
-            <Text weight="semibold">{currentDocument ? 'Insert Image' : 'Browse Assets'}</Text>
+        <Box flex={1} marginX={3}>
+          <TextEllipsis weight="semibold">
+            <span>{currentDocument ? 'Insert Image' : 'Browse Assets'}</span>
             {currentDocument && (
-              <Text>
-                <Icon symbol="arrow-right" />
-              </Text>
+              <>
+                <span
+                  style={{
+                    margin: '0 0.5em'
+                  }}
+                >
+                  <Icon symbol="arrow-right" />
+                </span>
+                <span
+                  style={{
+                    textTransform: 'capitalize'
+                  }}
+                >
+                  {currentDocument._type}
+                </span>
+              </>
             )}
-            {currentDocument && (
-              <Text
-                style={{
-                  textTransform: 'capitalize'
-                }}
-              >
-                {currentDocument._type}
-              </Text>
-            )}
-          </Inline>
+          </TextEllipsis>
         </Box>
 
         {/* Close */}
-        <Box marginX={3}>
+        <Box
+          marginX={3}
+          style={{
+            flexShrink: 0
+          }}
+        >
           <Button
             disabled={!onClose}
             icon={CloseIcon}
             mode="bleed"
             onClick={onClose}
-            radius={0}
+            radius={2}
             style={{
               opacity: onClose ? 1 : 0
             }}
