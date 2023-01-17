bd3c0ba7d40026f93ea0f2907c6bcc3e57c0487e
RostiMelk <hello@rosti.no>
2023-01-17 16:01:55 +0100

    custom name

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import ImageIcon from 'part:@sanity/base/image-icon'
 import AssetBrowser from './app'
 
 export default {
-  name: 'media',
-  title: 'Media',
+  name: 'media-rosti',
+  title: 'Media (Rostis version)',
   component: AssetBrowser,
   icon: ImageIcon
 }
