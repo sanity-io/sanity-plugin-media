6ec15e5f39b2633eff20a278314ab8d8874c2167
David Oluwasusi <daviddamilola20@gmail.com>
2023-09-20 19:38:10 +0100

    feat(asset): add lib to build

---
+++ lib/src/index.d.ts
@@ -0,0 +1,8 @@
+import type {AssetSource} from 'sanity'
+import {Plugin as Plugin_2} from 'sanity'
+
+export declare const media: Plugin_2<void>
+
+export declare const mediaAssetSource: AssetSource
+
+export {}
