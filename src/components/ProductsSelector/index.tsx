de50e9f2d93893eaa6a9c2065ece98af786710ae
David Oluwasusi <daviddamilola20@gmail.com>
2023-09-24 22:59:51 +0100

    fix(assest): load collaborations from

--- src/components/ProductsSelector/index.tsx
+++ src/components/ProductsSelector/index.tsx
@@ -91,7 +91,6 @@ export default function ProductSelector(props: {
         const updatedValue = [...localValue, productToAdd]
         setLocalValue(updatedValue)
         onChange?.(updatedValue)
-        setSearchValue('')
       }
     },
     [products, value, localValue, onChange]
