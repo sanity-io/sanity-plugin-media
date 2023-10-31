00766d3efba88a4ac46b4636e5436872d24dfe79
David Oluwasusi <daviddamilola20@gmail.com>
2023-10-31 14:19:29 +0100

    feat(mass-edit): fix mass edit without overrites

--- src/utils/getCollaborationSelectOptions.ts
+++ src/utils/getCollaborationSelectOptions.ts
@@ -4,7 +4,7 @@ import {CollaborationItem} from '../modules/collaborations'
 const getSeasonCollaborationOptions = (tags: CollaborationItem[]): SeasonSelectOption[] => {
   return tags.reduce((acc: SeasonSelectOption[], val) => {
     const collaboration = val?.collaboration
-    if (collaboration) {
+    if (collaboration && collaboration?.name) {
       acc.push({
         label: collaboration?.name?.current,
         value: collaboration?._id
