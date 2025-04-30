2367d8734357f2657b75651a9ab1beff8132a8cb
William Li <email@williamli.dev>
2025-04-30 11:41:47 +0800

    fix: fixed grid view only show 1 item bug Added forwardRef to fix virtualized grid item and list container refs

--- src/components/AssetGridVirtualized/index.tsx
+++ src/components/AssetGridVirtualized/index.tsx
@@ -1,5 +1,5 @@
 import type {CardAssetData, CardUploadData} from '../../types'
-import {memo} from 'react'
+import {memo, forwardRef} from 'react'
 import {VirtuosoGrid} from 'react-virtuoso'
 import {styled} from 'styled-components'
 import useTypedSelector from '../../hooks/useTypedSelector'
@@ -33,11 +33,11 @@ const StyledItemContainer = styled.div`
   width: ${CARD_WIDTH}px;
 `
 
-function ItemContainer(props: any) {
+const ItemContainer = forwardRef<HTMLDivElement, any>((props, ref) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars -- we're doing this to avoid sc warnings about `context` passed as an attribute
   const {context, ...rest} = props
-  return <StyledItemContainer {...rest} />
-}
+  return <StyledItemContainer ref={ref} {...rest} />
+})
 
 const StyledListContainer = styled.div`
   display: grid;
@@ -46,11 +46,12 @@ const StyledListContainer = styled.div`
   justify-content: center;
   margin: 0 auto;
 `
-function ListContainer(props: any) {
+
+const ListContainer = forwardRef<HTMLDivElement, any>((props, ref) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars -- we're doing this to avoid sc warnings about `context` passed as an attribute
   const {context, ...rest} = props
-  return <StyledListContainer {...rest} />
-}
+  return <StyledListContainer ref={ref} {...rest} />
+})
 
 const AssetGridVirtualized = (props: Props) => {
   const {items, onLoadMore} = props
