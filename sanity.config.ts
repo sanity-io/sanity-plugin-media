12de376e36186383c39c003082519f21c118ba16
pedrobonamin <pedrobonamin@gmail.com>
2026-05-11 14:26:34 +0200

    fix: use useBrowserInit in Browser

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,9 +1,29 @@
 import {defineConfig} from 'sanity'
-import {media} from './src'
+import {mediaField, media} from './src'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
 
 export default defineConfig({
   name: 'sanity-plugin-media',
   projectId: 'ppsg7ml5',
   dataset: 'test',
-  plugins: [media()]
+  schema: {
+    types: [
+      {
+        name: 'product',
+        title: 'Product',
+        type: 'document',
+        fields: [
+          {name: 'name', title: 'Name', type: 'string'},
+          mediaField({
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            mediaTags: ['product']
+          })
+        ]
+      }
+    ]
+  },
+  plugins: [media(), structureTool({}), visionTool()]
 })
--- src/components/Browser/index.tsx
+++ src/components/Browser/index.tsx
@@ -1,15 +1,8 @@
-import type {MutationEvent} from '@sanity/client'
 import {Card, Flex, PortalProvider} from '@sanity/ui'
-import type {Asset, Tag} from '../../types'
-import groq from 'groq'
-import {useEffect, useState} from 'react'
-import {useDispatch} from 'react-redux'
+import {useState} from 'react'
 import {type AssetSourceComponentProps, type SanityDocument} from 'sanity'
-import {TAG_DOCUMENT_NAME} from '../../constants'
 import {AssetBrowserDispatchProvider} from '../../contexts/AssetSourceDispatchContext'
 import useVersionedClient from '../../hooks/useVersionedClient'
-import {assetsActions} from '../../modules/assets'
-import {tagsActions} from '../../modules/tags'
 import GlobalStyle from '../../styled/GlobalStyles'
 import Controls from '../Controls'
 import DebugControls from '../DebugControls'
@@ -21,6 +14,7 @@ import PickedBar from '../PickedBar'
 import ReduxProvider from '../ReduxProvider'
 import TagsPanel from '../TagsPanel'
 import UploadDropzone from '../UploadDropzone'
+import {useBrowserInit} from './useBrowserInit'
 
 type Props = {
   assetType?: AssetSourceComponentProps['assetType']
@@ -28,71 +22,20 @@ type Props = {
   onClose?: AssetSourceComponentProps['onClose']
   onSelect?: AssetSourceComponentProps['onSelect']
   selectedAssets?: AssetSourceComponentProps['selectedAssets']
+  schemaType?: AssetSourceComponentProps['schemaType']
 }
 
-const BrowserContent = ({onClose}: {onClose?: AssetSourceComponentProps['onClose']}) => {
+const BrowserContent = ({
+  onClose,
+  schemaType
+}: {
+  onClose?: AssetSourceComponentProps['onClose']
+  schemaType?: AssetSourceComponentProps['schemaType']
+}) => {
   const client = useVersionedClient()
   const [portalElement, setPortalElement] = useState<HTMLDivElement | null>(null)
-  const dispatch = useDispatch()
 
-  useEffect(() => {
-    const handleAssetUpdate = (update: MutationEvent) => {
-      const {documentId, result, transition} = update
-
-      if (transition === 'appear') {
-        dispatch(assetsActions.listenerCreateQueue({asset: result as Asset}))
-      }
-
-      if (transition === 'disappear') {
-        dispatch(assetsActions.listenerDeleteQueue({assetId: documentId}))
-      }
-
-      if (transition === 'update') {
-        dispatch(assetsActions.listenerUpdateQueue({asset: result as Asset}))
-      }
-    }
-
-    const handleTagUpdate = (update: MutationEvent) => {
-      const {documentId, result, transition} = update
-
-      if (transition === 'appear') {
-        dispatch(tagsActions.listenerCreateQueue({tag: result as Tag}))
-      }
-
-      if (transition === 'disappear') {
-        dispatch(tagsActions.listenerDeleteQueue({tagId: documentId}))
-      }
-
-      if (transition === 'update') {
-        dispatch(tagsActions.listenerUpdateQueue({tag: result as Tag}))
-      }
-    }
-
-    // Fetch assets: first page
-    dispatch(assetsActions.loadPageIndex({pageIndex: 0}))
-
-    // Fetch all tags
-    dispatch(tagsActions.fetchRequest())
-
-    // Listen for asset and tag changes in published documents.
-    // Remember that Sanity listeners ignore joins, order clauses and projections!
-    // Also note that changes to the selected document (if present) will automatically re-load the media plugin
-    // due to the desk pane re-rendering.
-    const subscriptionAsset = client
-      .listen(
-        groq`*[_type in ["sanity.fileAsset", "sanity.imageAsset"] && !(_id in path("drafts.**"))]`
-      )
-      .subscribe(handleAssetUpdate)
-
-    const subscriptionTag = client
-      .listen(groq`*[_type == "${TAG_DOCUMENT_NAME}" && !(_id in path("drafts.**"))]`)
-      .subscribe(handleTagUpdate)
-
-    return () => {
-      subscriptionAsset?.unsubscribe()
-      subscriptionTag?.unsubscribe()
-    }
-  }, [client, dispatch])
+  useBrowserInit(client, schemaType)
 
   return (
     <PortalProvider element={portalElement}>
@@ -137,7 +80,7 @@ const Browser = (props: Props) => {
     >
       <AssetBrowserDispatchProvider onSelect={props?.onSelect}>
         <GlobalStyle />
-        <BrowserContent onClose={props?.onClose} />
+        <BrowserContent onClose={props?.onClose} schemaType={props?.schemaType} />
       </AssetBrowserDispatchProvider>
     </ReduxProvider>
   )
